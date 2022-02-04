cec72094bb5de551baeeb4ed2db023b323831fea
alinabuka <bukanbaevaalina@gmail.com>
2022-02-04 17:53:47 +0300

    6. Checkstyle [#4123]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,8 +1,9 @@
 package ru.job4j;
 
 public class StudentInfo {
-    public static void main(String []args) {
-        System.out.println ("Alina Bukanbaeva");
-        System.out.println ("13.03.2002");
+    @SuppressWarnings("checkstyle:NoWhitespaceAfter")
+    public static void main(String[] args) {
+        System.out.println("Alina Bukanbaeva");
+        System.out.println("13.03.2002");
     }
 }
