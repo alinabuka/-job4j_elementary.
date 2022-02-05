05025f4d6a36d6f52c49840b0e646c795491de88
alinabuka <bukanbaevaalina@gmail.com>
2022-02-05 16:27:44 +0300

    0. Что такое переменная. [#3610]

---
+++ src/main/java/ru/job4j/Variables.java
@@ -0,0 +1,12 @@
+package ru.job4j;
+
+public class Variables {
+    public static void main(String[] args) {
+        int age = 18;
+        System.out.println(age);
+        double length = 78.0332;
+        System.out.println(length);
+        boolean baby = true;
+        System.out.println(baby);
+    }
+}
